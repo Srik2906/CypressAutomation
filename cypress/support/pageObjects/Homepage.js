f38a5ccbe7f2c9ac26d2721d44f8f73143160a44
srikanth <srikanth.krishnan@happyfox.com>
2022-06-10 11:34:27 +0530

    mocha reports

--- cypress/support/pageObjects/Homepage.js
+++ cypress/support/pageObjects/Homepage.js
@@ -12,20 +12,19 @@ class HomePage {
     }
 
     getGender() {
-    return cy.get('#exampleFormControlSelect1')
+        return cy.get('#exampleFormControlSelect1')
     }
 
     getRadioone() {
-        return  cy.get('#inlineRadio2')
+        return cy.get('#inlineRadio2')
     }
 
     getDataBinding() {
         return cy.get(':nth-child(4) > .ng-valid')
     }
 
-    getShop()
-    {
-       return  cy.get(':nth-child(2) > .nav-link')
+    getShop() {
+        return cy.get(':nth-child(2) > .nav-link')
     }
 
 
