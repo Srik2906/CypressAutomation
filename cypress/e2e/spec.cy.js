c0d159e81111322977f7b8c74ac04659be771341
srikanth <srikanth.krishnan@happyfox.com>
2022-06-20 12:45:05 +0530

    fixed spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,7 @@
 
 describe("Test suite", () => {
   it("Test case", () => {
-    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/)
+    cy.visit(Cypress.env('url') + '/seleniumPractise')
     cy.get('input.search-keyword').type('ca')
     cy.get('.products').as('productLocator')
     cy.get('@productLocator').find('.product').should('have.length', 4)
@@ -20,6 +20,15 @@ describe("Test suite", () => {
      cy.get('.cart-icon > img').click()
      cy.contains('PROCEED TO CHECKOUT').click()
      cy.contains('Place Order').click()
+     cy.get('select').select("India")
+     cy.get('.chkAgree').click()
+     cy.get('button').click()
+     cy.get('div.wrapperTwo span:nth-child(1)').then((message) =>
+     { 
+       let successMessage=message.text()
+       expect(successMessage.includes('Thank you, your order has been placed successfully ')).to.be.true
+     })
+
   }
   )
 })
